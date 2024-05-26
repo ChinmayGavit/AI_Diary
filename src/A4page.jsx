102f84a4a489c82c30ccbf7bd933ba40af1b86b2
ChinmayGavit <chinmaygavit1373@gmail.com>
2024-05-26 22:13:43 +0530

    Frontend

--- src/A4page.jsx
+++ src/A4page.jsx
@@ -4,12 +4,13 @@ import { Tldraw } from 'tldraw';
 
 const A4Container = styled.div`
   width: 210mm;
-  height: 297mm;
+  height: calc(100vh - 40px); /* Adjusting height based on window height */
   border: 1px solid #000;
   margin: 20px auto;
   background-color: #fff;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   page-break-after: always;
+  overflow: hidden; /* Ensure no scrollbars */
 `;
 
 const TldrawContainer = styled.div`
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import A4pages from './A4pages.jsx'
+import {Route, BrowserRouter,Routes} from 'react-router-dom';
+import Signin from './component/Signin';
+import Nopage from './component/Nopage';
+import Signup from './component/Signup';
+import Heropage from './component/Heropage.jsx';
 
-import A4pages from './A4pages.jsx';
 
-const App = () => {
+export default function App() {
   return (
     <div>
-      <h1>AI Image Diary</h1>
-      <div>
-      <A4pages />
-
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Heropage/>}/>
+          <Route path="/create" element={<Nopage/>}/>
+          <Route path="/signin" element={<Signup/>}/>
+          <Route path="/login" element={<Signin/>}/>
+          <Route path="/customizepage" element={<A4pages/>}/>
+          <Route path="/ai" element={<Nopage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
-};
-
-
-
-export default App;
+    // <A4page/>
+  )
+}
